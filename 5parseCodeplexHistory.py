ce1024d22bfb8111f1ba37409d3098f2de949ae4
Megan Squire <megansquire@gmail.com>
2017-06-12 11:01:03 -0400

    Create 5parseCodeplexHistory.py
    added month converter, small formatting changes

--- 5parseCodeplexHistory.py
+++ 5parseCodeplexHistory.py
@@ -26,101 +26,125 @@
 #
 ################################################################
 # usage:
-# 1getCodeplexPages.py <datasource_id> <db password>
+# 5parseCodeplexHistory.py <datasource_id> <db password>
 
 # purpose:
-# grab all the authors, author urls, pages, and page url of page updates for projects stored on Codeplex before it was shut down
+# parse history pages for projects stored on Codeplex before it was shut down
 ################################################################
 
 import pymysql
 from bs4 import BeautifulSoup
 import sys
-import getpass
-import datetime
-import re
 
-pw=getpass.getpass()
-
-# grab commandline args
-datasourceID = 70910 
-last_updated = None
+datasourceID = sys.argv[1]
+password     = sys.argv[2]
 
 page_name = None
 page_url = None
 author = None
-author_url= None
+author_url = None
 
 # Open remote database connection
 dbconn = pymysql.connect(host='',
                          user='',
-                         passwd=pw,
-                         db='',
+                         passwd=password,
+                         db='codeplex',
                          use_unicode=True,
-                         charset="utf8mb4",
+                         charset='utf8mb4',
                          autocommit=True)
 cursor = dbconn.cursor()
-
 
 selectProjects = 'SELECT proj_name \
                   FROM cp_projects_indexes \
                   WHERE datasource_id = %s'
-                  
-selectIndexes = 'SELECT history_html \
+
+selectIndex = 'SELECT history_html \
                  FROM cp_projects_indexes \
                  WHERE datasource_id = %s \
-                 AND proj_name = %s' 
-                 
-insertProjects = 'INSERT INTO cp_project_historyC \
-                  values(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s)'            
-             
+                 AND proj_name = %s'
+
+insertProjectHistory = 'INSERT INTO cp_project_history (history_id, \
+                                                  proj_name, \
+                                                  datasource_id, \
+                                                  month, \
+                                                  year, \
+                                                  page_name, \
+                                                  page_url, \
+                                                  author, \
+                                                  author_url, \
+                                                  last_updated) \
+                        VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, now())'
+
+
+def convertMonth(moName):
+    months = {'January': '01',
+              'February': '02',
+              'March': '03',
+              'April': '04',
+              'May': '05',
+              'June': '06',
+              'July': '07',
+              'August': '08',
+              'September': '09',
+              'October': '10',
+              'November': '11',
+              'December': '12'}
+    return months[moName]
 
 # grab the project list
+print('Selecting projects...')
 cursor.execute(selectProjects, (datasourceID,))
 projectList = cursor.fetchall()
 
-
 for project in projectList:
     projectName = project[0]
-    print("parsing:", projectName)
-    
-    # grab the index
-    cursor.execute(selectIndexes, (datasourceID, projectName))
+    print("parsing project:", projectName)
+
+    # grab the history page
+    cursor.execute(selectIndex, (datasourceID, projectName))
     historyHtml = cursor.fetchone()[0]
 
-    #grab the history page we need 
     soup = BeautifulSoup(historyHtml, "html.parser")
     divs = soup.find_all("div", id="DateDiv")
 
     if divs:
         for d in divs:
-            # get the h2 contents that contain information on the date of updates 
+            # get the h2 contents containing date information
             dateList = d.h2.contents
             # get dates and separate them by month and year
             for l in dateList:
-                date = l
                 dateSplit = l.split(",")
-                month = dateSplit[0]
+                monthName = dateSplit[0]
                 year = dateSplit[1]
-                
-                #gets information on  user and page informaiton for each update
+
+                # to-do: convert month to 2-digit character
+                monthNum = convertMonth(monthName)
+                # get information on  user and page information for each update
                 authorWholeList = d.find_all('tr')
 
-                i=0
+                i = 0
                 for line in authorWholeList:
                     authorList = line.find('td')
 
                     if authorList:
                         for section in authorList:
                             # get the author name and author url
-                            author = line.find(id=('ModifiedByLiteral' + str(i))).contents[0]
-                            authorUrl = line.find(id=('ModifiedByLiteral' + str(i)))['href']
-                            
+                            author = line.find(id=('ModifiedByLiteral' +
+                                                   str(i))).contents[0]
+                            authorUrl = line.find(id=('ModifiedByLiteral' +
+                                                      str(i)))['href']
                             # get the page name and page url
-                            page = line.find(id=('PageTitleLink' + str(i))).contents[0]
-                            pageUrl = line.find(id=('PageTitleLink' + str(i)))['href']
-
-                            i = i+1  
-                            last_updated = datetime.datetime.now()
-                            
-                            cursor.execute(insertProjects, (projectName, datasourceID, month, year, page, pageUrl, author, authorUrl, last_updated))
+                            page = line.find(id=('PageTitleLink' +
+                                                 str(i))).contents[0]
+                            pageUrl = line.find(id=('PageTitleLink' +
+                                                    str(i)))['href']
+                            cursor.execute(insertProjectHistory, (projectName,
+                                                                  datasourceID,
+                                                                  monthNum,
+                                                                  year,
+                                                                  page,
+                                                                  pageUrl,
+                                                                  author,
+                                                                  authorUrl))
+                            i += 1
 dbconn.close()
