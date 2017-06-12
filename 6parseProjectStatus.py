58062187357a329ff3df0d40618bf88e6d367d8a
Megan Squire <megansquire@gmail.com>
2017-06-12 10:17:14 -0400

    Create 6parseProjectStatus.py

--- 6parseProjectStatus.py
+++ 6parseProjectStatus.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 12 09:46:28 2017
-
-@author: megan
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # This program is free software; you can redistribute it
 # and/or modify it under the terms of GPL v3
 #
